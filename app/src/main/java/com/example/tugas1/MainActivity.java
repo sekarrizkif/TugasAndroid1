6156b6a394560642d2891456f29ab1f22d3707f9
sekarrizkif <sekarrhizky29@gmail.com>
2020-02-05 13:32:35 +0700

    Ini Tugasnya

---
+++ app/src/main/java/com/example/tugas1/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.tugas1;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
